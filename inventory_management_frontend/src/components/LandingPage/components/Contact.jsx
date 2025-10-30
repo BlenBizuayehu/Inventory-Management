516f22aff006cf884f6861629235a40176b145b9
blen bizuayehu <blenaqua57js@gmail.com>
2025-10-30 23:45:01 +0300

    fixed contact typo

--- inventory_management_frontend/src/components/LandingPage/components/Contact.jsx
+++ inventory_management_frontend/src/components/LandingPage/components/Contact.jsx
@@ -1,4 +1,4 @@
-import './Contact.css'; // Assuming you'll add styles in this file
+import './contact.css'; // Assuming you'll add styles in this file
 import ContactUs from './ContactUs';
 import MapSection from './MapSection';
 
