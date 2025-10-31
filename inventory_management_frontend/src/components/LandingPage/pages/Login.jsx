1509288781966b115327bd8d99e044b3885f9468
blen bizuayehu <blenaqua57js@gmail.com>
2025-10-31 22:58:48 +0300

    fixed api import

--- inventory_management_frontend/src/components/LandingPage/pages/Login.jsx
+++ inventory_management_frontend/src/components/LandingPage/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api"; // ✅ import your axios instance
+import api from "../../../api";
 import "./Login.css";
 
 function Login() {
